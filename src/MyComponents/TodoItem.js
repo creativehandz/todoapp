a2d0cabaf548a8893661fa77fe5c59f36a9bc94c
Pranav <prasharpranav@gmail.com>
2021-07-29 22:03:09 +0530

    timer button added

--- src/MyComponents/TodoItem.js
+++ src/MyComponents/TodoItem.js
@@ -1,18 +1,24 @@
 import React from 'react'
 
-export const TodoItem = ({todo, onDelete}) => {
+export const TodoItem = ({ todo, onDelete }) => {
     return (
         <>
-        
-        <div className="col-4 mb-5">
-            <div className="card bg-light shadow">
-                <div className="card-body">
-           <h4 className="card-title">{todo.title}</h4>
-           <p className="card-text">{todo.desc}</p>
-           <button className="btn btn-sm btn-danger" onClick={()=>{onDelete(todo)}}>Delete</button> 
-        </div>
-        </div>
-        </div>
+
+            <div className="col-4 mb-5">
+                <div className="card bg-light shadow">
+                    <div className="card-body">
+                        <h4 className="card-title">{todo.title}</h4>
+                        <p className="card-text">{todo.desc}</p>
+                        <div className="d-flex justify-content-start">
+                        <button className="btn btn-sm btn-success" >Start</button>
+                        <div className="timer px-2">0:22</div>
+                        
+                        <button className="btn btn-sm btn-danger ms-auto" onClick={() => { onDelete(todo) }}>Delete</button>
+                        </div>
+                        
+                    </div>
+                </div>
+            </div>
         </>
     )
 }
